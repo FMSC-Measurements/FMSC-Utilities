bba98feb486f3481e9fa2d82f11e8b21260b1735
bmillerfs <bryan.c.miller@usda.gov>
2021-09-08 18:27:50 -0600

    Update FileUtils.java

--- src/main/java/com/usda/fmsc/utilities/FileUtils.java
+++ src/main/java/com/usda/fmsc/utilities/FileUtils.java
@@ -4,11 +4,13 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.channels.FileChannel;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class FileUtils {
@@ -171,5 +173,31 @@ public class FileUtils {
             out.write(data, 0, count);
         }
         out.closeEntry();
+    }
+
+
+    public static void unzip(File zipFile, File targetDirectory) throws IOException {
+        try (ZipInputStream zis = new ZipInputStream(
+                new BufferedInputStream(new FileInputStream(zipFile)))) {
+            ZipEntry ze;
+            int count;
+            byte[] buffer = new byte[8192];
+            while ((ze = zis.getNextEntry()) != null) {
+                File file = new File(targetDirectory, ze.getName());
+                File dir = ze.isDirectory() ? file : file.getParentFile();
+                if (!dir.isDirectory() && !dir.mkdirs())
+                    throw new FileNotFoundException("Failed to ensure directory: " +
+                            dir.getAbsolutePath());
+                if (ze.isDirectory())
+                    continue;
+                try (FileOutputStream fout = new FileOutputStream(file)) {
+                    while ((count = zis.read(buffer)) != -1)
+                        fout.write(buffer, 0, count);
+                }
+                long time = ze.getTime();
+                if (time > 0)
+                    file.setLastModified(time);
+            }
+        }
     }
 }
